6873bf0711a0f630a46f1c48f97b553687dfd2e8
Alan Dominguez <edgar.dominguez5434@alumnos.udg.mx>
2021-10-21 00:11:49 -0500

    Correction url footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer id="footer" class="bg-dark text-center text-lg-start">
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(32, 29, 29, 0.404);">
-      <a class="text-light" href="https://mdbootstrap.com/">© 2021 Copyright: BookStore</a>
+      <a class="text-light" href="https://github.com/edgardominguez23/bookstore">© 2021 Copyright: BookStore</a>
     </div>
     <!-- Copyright -->
 </footer>
